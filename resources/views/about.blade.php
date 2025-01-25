0f045a506f60e99b79992c71009a04b7d280c6ee
Rine <andrewmagtibay21@gmail.com>
2025-01-25 09:37:50 +0800

    Day 3 & 4

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:heading>
-        Contact Page
+        About Page
     </x-slot:heading>
     
-    <h1>Hello from Contact</h1>
+    <h1>Hello from About page</h1>
 </x-layout>
